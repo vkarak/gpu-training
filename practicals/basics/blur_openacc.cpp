c28c8b2748ef1d699b925d3674658f33efc2dbdd
Radim Janalík <radim.janalik@gmail.com>
2018-05-15 00:04:08 +0200

    Blur example with copying pointers instead of the content of the array
    Implemented only the function _nocopies
    This should be in the solution, not here
    copy(out) could be avoided, but it would be complicated

--- practicals/basics/blur_openacc.cpp
+++ practicals/basics/blur_openacc.cpp
@@ -69,24 +69,30 @@ void blur_twice_gpu_naive(double *in , double *out , int n, int nsteps)
 void blur_twice_gpu_nocopies(double *in , double *out , int n, int nsteps)
 {
     double *buffer = malloc_host<double>(n);
+    double *in_bk = in;
 
     // TODO: move the data needed by the algorithm to the GPU
+#pragma acc data copyin(in_bk[0:n]), copy(out[0:n]), create(buffer[0:n])
     {
         for (auto istep = 0; istep < nsteps; ++istep) {
             // TODO: offload this loop to the GPU
+#pragma acc parallel loop present(in, buffer)
             for (auto i = 1; i < n-1; ++i) {
                 buffer[i] = blur(i, in);
             }
 
             // TODO: offload this loop to the GPU
+#pragma acc parallel loop present(out, buffer)
             for (auto i = 2; i < n-2; ++i) {
                 out[i] = blur(i, buffer);
             }
 
-            // TODO: offload this loop to the GPU; can you try just the pointer assignment?
-            for (auto i = 0; i < n; ++i) {
-                in[i] = out[i];
-            }
+//            // TODO: offload this loop to the GPU; can you try just the pointer assignment?
+//#pragma acc parallel loop present(in, out)
+//            for (auto i = 0; i < n; ++i) {
+//                in[i] = out[i];
+//            }
+            in = out;
         }
     }
 
